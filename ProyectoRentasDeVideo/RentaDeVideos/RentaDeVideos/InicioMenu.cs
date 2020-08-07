d11a63681d0a274fd5c2c386266700412a14d14a
CYPAMR99 <cmedinar1@miumg.edu.gt>
2020-08-06 21:22:43 -0600

    Primer Proyecto Formularios

---
+++ ProyectoRentasDeVideo/RentaDeVideos/RentaDeVideos/InicioMenu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentaDeVideos
+{
+    public partial class FormularioInicioMenu : Form
+    {
+        public FormularioInicioMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mantenimientos.Clientes.FormularioIngreso_Cliente form = new Mantenimientos.Clientes.FormularioIngreso_Cliente();
+            form.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Mantenimientos.Proveedores.FormularioIngreso_Proveedor prov = new Mantenimientos.Proveedores.FormularioIngreso_Proveedor();
+            prov.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Mantenimientos.Usuarios.FormularioIngreso_Usuario user = new Mantenimientos.Usuarios.FormularioIngreso_Usuario();
+            user.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Mantenimientos.Cargos.FormularioIngreso_Cargo cargo = new Mantenimientos.Cargos.FormularioIngreso_Cargo();
+            cargo.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Mantenimientos.Empleados.FormularioIngreso_Empleado emp = new Mantenimientos.Empleados.FormularioIngreso_Empleado();
+            emp.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ ProyectoRentasDeVideo/RentaDeVideos/RentaDeVideos/Mantenimientos/Clientes/IngresoClientes.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace RentaDeVideos.Mantenimientos.Clientes
+{
+    public partial class IngresoClientes : Form
+    {
+        public IngresoClientes()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void picSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void picMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void picBotonMenuSlide_Click(object sender, EventArgs e)
+        {
+            if (pnlSlideMenu.Width == 188)
+            {
+                pnlSlideMenu.Width = 40;
+            }
+            else
+                pnlSlideMenu.Width = 188;
+        }
+
+        private void pnlFormMenu_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnClientes_Click(object sender, EventArgs e)
+        {
+            FormularioIngreso_Cliente fic = new FormularioIngreso_Cliente();
+            fic.Show();
+            this.Hide();
+        }
+
+        private void btnIngreso_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Esta dentro de esa ventana", "Atencion", MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+        }
+
+        private void btnVolverMenu_Click(object sender, EventArgs e)
+        {
+            FormularioInicioMenu fim = new FormularioInicioMenu();
+            fim.Show();
+            this.Hide();
+        }
+
+        private void btnAct_Eliminar_Click(object sender, EventArgs e)
+        {
+            ActualizarEliminarClientes aec = new ActualizarEliminarClientes();
+            aec.Show();
+            this.Hide();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            BuscarClientes bc = new BuscarClientes();
+            bc.Show();
+            this.Hide();
+        }
+
+        private void pnlContenido_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblApellidos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
